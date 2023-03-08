dc6b979f8b1a45e45109dae510149a629863014a
niteshlanjewar <nitlanjewar1621@gmail.com>
2023-03-08 15:58:50 +0530

    insta6

--- instagram/src/components/Model.js
+++ instagram/src/components/Model.js
@@ -3,13 +3,49 @@ import { ContextProvider } from '../Global/Context';
 
 const Model = () => {
   const {model} = React.useContext(ContextProvider);
+  const [state, setState] = React.useState({
+    register: true,
+    login: false
+  })
+  const formsToggle = ()=>{
+    setState({
+      ...state,
+      register: !state.register,
+      login: !state.login
+
+    })
+  };
   return (
     <>
 
     {model ? <div className='model'>
       <div className='model__container'>
-     <div className='model__container__forms'>
-<h1>hello user</h1>
+     <div className='model__form'>
+     <form>
+      <div className='model__group'>
+     <img src='/images/instagramLogo.png' alt='' />
+      </div>
+      <div className='model__group'>
+        <input type="text" name="username" class="model__input" placeholder='Username...' />
+        </div>
+
+        <div className='model__group'>
+        <input type="email" name="email" class="model__input" placeholder='Email...' />
+        </div>
+       
+        <div className='model__group'>
+        <input type="password" name="password" class="model__input" placeholder='Create password...' />
+        </div>
+
+        <div className='model__group'>
+        <input type="submit" value="Register" class="btn btn-smart" />
+        </div>
+
+        <div className='model__group'>
+        <span onClick={formsToggle}>Already have an account ?</span>
+        </div>
+
+     </form>
      </div>
       </div>
     </div> : ""}
